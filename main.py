d3d162d9d5adcd2bc07761f95c1b17fd8144b165
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-11-28 16:45:31 +0530

    initial commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def match_words(words):
         if len(word) > 1 and word[0] == word[-1]:
             lst.append(word)
             ctr += 1
-        print("List of words with  1st and last character same\n", lst)
-        return ctr
+    print("List of words with  1st and last character same\n", lst)
+    return ctr
 count = match_words(['abc', 'cfc', 'xyz', 'aba', '1221'])
 print("Number of words having same first and last letters are:", count)
